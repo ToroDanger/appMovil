e06efc974a6aceb260e56dd3cfc1a9882ef4ad7d
benjvvp2001 <be.pezoa@duocuc.cl>
2024-09-06 22:46:53 -0400

    animacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras } from "@angular/router";
 import { NavController } from "@ionic/angular";
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +14,8 @@ export class HomePage implements OnInit {
     user: "",
     pw: "",
   };
-
-  constructor(private nCtrl:NavController ) {}
+  private animation!:Animation;
+  constructor(private nCtrl:NavController, private aCtrl:AnimationController ) {}
 
   goSegunda() {
     let nExtras:NavigationExtras = {
@@ -30,4 +31,20 @@ export class HomePage implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    this.animation = this.aCtrl.create()
+    .addElement(document.querySelector('.square') as HTMLElement)
+    .iterations(Infinity)
+    .duration(7000)
+    .keyframes([
+      { offset: 0, transform: 'translateX(0px) rotate(0deg)', opacity: '1' },
+      { offset: 0.5, transform: 'translateX(300px) rotate(180deg)', opacity: '1' },
+      { offset: 0.8, transform: 'translateX(600px) rotate(360deg)', opacity: '0.5 ' }
+    ]);
+    this.animation.play();
+  }
+
+
+
 }
+
