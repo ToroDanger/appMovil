dcb625bb40b8166c6686d5af881722fc0ffad1dc
benjvvp2001 <be.pezoa@duocuc.cl>
2024-09-10 18:20:39 -0300

    CAMBIO

--- src/app/info-viaje/info-viaje.page.ts
+++ src/app/info-viaje/info-viaje.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-info-viaje',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-viaje.page.scss'],
 })
 export class InfoViajePage implements OnInit {
-
-  constructor() { }
+  private animation!:Animation;
+  constructor(private aCtrl:AnimationController) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    this.animation = this.aCtrl.create()
+    .addElement(document.querySelector('.square') as HTMLElement)
+    .iterations(1)
+    .duration(1000)
+    .keyframes([
+      { offset: 0, transform: 'translateY(0px) rotate(0deg)', opacity: '1' },
+      { offset: 0.5, transform: 'translateY(300px) rotate(180deg)', opacity: '1' },
+      { offset: 0.8, transform: 'translateY(600px) rotate(360deg)', opacity: '0.5 ' }
+    ]);
+   
+  }
+  ejecutar(){
+    this.animation.play();
+  }
 }
