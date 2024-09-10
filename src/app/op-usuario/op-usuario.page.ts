f198ddea46401d92d26fe3ba1d6be2d4e1ea9f37
ToroDanger <bryanpmg90@gmail.com>
2024-09-10 18:34:36 -0300

    Animacion op-usuario

--- src/app/op-usuario/op-usuario.page.ts
+++ src/app/op-usuario/op-usuario.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-op-usuario',
@@ -11,14 +12,27 @@ export class OpUsuarioPage implements OnInit {
     user:"",
     pw:""
   }
+  private animation!:Animation;
 
-  constructor(private aRoute:ActivatedRoute) { }
+  constructor(private aRoute:ActivatedRoute, private aCtrl:AnimationController ) { }
 
   ngOnInit() {
     this.aRoute.queryParams.subscribe(params => {
       this.diccionario.user = params['dataUser']
       this.diccionario.pw = params['dataPw']
     });
+  }
+  ngAfterViewInit(){
+    this.animation = this.aCtrl.create()
+    .addElement(document.querySelector('.square') as HTMLElement)
+    .iterations(Infinity)
+    .duration(7000)
+    .keyframes([
+      { offset: 0, transform: 'translateY(0px) rotate(0deg)', opacity: '1' },
+      { offset: 0.5, transform: 'translateY(0px) rotate(360deg)', opacity: '1' },
+      { offset: 0.8, transform: 'translateY(0px) rotate(360deg)', opacity: '0.5 ' }
+    ]);
+    this.animation.play();
   }
 
 }
